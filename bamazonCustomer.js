a9f5f4025367cea63524774a5f400e79c423dd66
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-04-14 09:51:33 -0700

    First promt working, all items function next

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,60 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon_db"
 });
+
+menuStart();
+
+function menuStart() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "You're connected to Bamazon! Lets get shopping!",
+                choices: ["See all items"],
+                name: "shop"
+            },
+        ])
+        .then(function (data) {
+            //console.log(data.shop);
+            if (data.shop === "See all items") {
+                inquirer
+                    .prompt([
+                        {
+                            type: "list",
+                            message: "What type of items would you like to view?",
+                            choices: ["All Items", "Electronics", "Kitchen", "Office", "Furniture", "Home"],
+                            name: "myItems"
+                        }
+                    ])
+                    .then(function (data) {
+                        connection.connect(function (err) {
+                            switch (data.myItems) {
+                                case "All Items":
+                                    allItems();
+                                    break;
+                                case "Electronics":
+                                    electronicsItems();
+                                    break;
+                                case "Kitchen":
+                                    kitchenItems();
+                                    break;
+                                case "Sports":
+                                    sportsItems();
+                                    break;
+                                case "Auto":
+                                    autoItems();
+                                    break;
+                                case "Bathroom":
+                                    bathroomItems();
+                                    break;
+                            }
+                            connection.end();
+                        });
+                    })
+            }
+        });
+}
+
+function allItems() {
+
+}
